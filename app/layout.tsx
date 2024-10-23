c3d4384c51fb6333966dd221dec4ec5175b229fd
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-10-23 20:04:09 +0800

    feat: add a topbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Toaster } from 'react-hot-toast'
 import { Providers } from './providers'
+import { KunTopBar } from '~/components/kun/TopBar'
 import '~/styles/index.css'
 
 export default function RootLayout({
@@ -10,9 +11,12 @@ export default function RootLayout({
   return (
     <html lang="zh-CN" suppressHydrationWarning>
       <body>
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
-          <Providers>{children}</Providers>
-          <Toaster />
+        <div className="relative flex flex-col min-h-screen bg-white bg-radial">
+          <KunTopBar />
+          <div className="flex items-center justify-center min-h-[calc(100dvh-64px)]">
+            <Providers>{children}</Providers>
+            <Toaster />
+          </div>
         </div>
       </body>
     </html>
