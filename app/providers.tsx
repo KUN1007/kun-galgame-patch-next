a073a5ec1ed89a7779841d9765adac1e3d810226
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-10-24 01:37:59 +0800

    feat: add a loading progress

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { AppProgressBar } from 'next-nprogress-bar'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider } from 'next-themes'
 
@@ -7,6 +8,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
       <ThemeProvider attribute="class">{children}</ThemeProvider>
+      <AppProgressBar
+        height="4px"
+        color="#006fee"
+        options={{ showSpinner: false }}
+      />
     </NextUIProvider>
   )
 }
