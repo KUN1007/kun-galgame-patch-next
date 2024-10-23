8f96d0385b7592f3dd53c2cdf2e8fd9c01caec33
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-10-24 00:49:07 +0800

    feat: handler error, prisma context

--- lib/trpc.ts
+++ lib/trpc.ts
@@ -1,8 +1,19 @@
 import { initTRPC } from '@trpc/server'
 import superjson from 'superjson'
+import { ZodError } from 'zod'
+import { createContext } from '~/server/context'
 
-const t = initTRPC.create({
-  transformer: superjson
+const t = initTRPC.context<typeof createContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError: error.cause instanceof ZodError ? error.cause.flatten() : null
+      }
+    }
+  }
 })
 
 export const router = t.router
--- server/context.ts
+++ server/context.ts
@@ -1,4 +1,5 @@
 import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch'
+import { prisma } from '~/prisma/index'
 
 export type Context = Awaited<ReturnType<typeof createContext>>
 
@@ -6,10 +7,11 @@ export type Context = Awaited<ReturnType<typeof createContext>>
  * Creates context for an incoming request
  * @see https://trpc.io/docs/v11/context
  */
-export async function createContext(opts?: FetchCreateContextFnOptions) {
+export const createContext = (opts?: FetchCreateContextFnOptions) => {
   // for API-response caching see https://trpc.io/docs/v11/caching
 
   return {
+    prisma,
     headers: opts && Object.fromEntries(opts.req.headers)
   }
 }
