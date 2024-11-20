0161cb1b95a8601a0c706990edf86a46ca205305
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-11-20 18:40:21 +0800

    feat: auto scroll document to top

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,17 @@
 'use client'
 
+import { useEffect } from 'react'
 import { AppProgressBar } from 'next-nprogress-bar'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider } from 'next-themes'
+import { usePathname } from 'next/navigation'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  useEffect(() => {
+    window.scroll(0, 0)
+  }, [pathname])
+
   return (
     <NextUIProvider>
       <ThemeProvider attribute="class">{children}</ThemeProvider>
