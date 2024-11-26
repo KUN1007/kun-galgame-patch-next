78778b0613a56905d1f8a9edae3c53d7b195a239
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-11-26 21:04:38 +0800

    fix: upload resource

--- server/routers/patch/resource/link.ts
+++ server/routers/patch/resource/link.ts
@@ -16,65 +16,63 @@ export const createOrUpdatePatchResourceLink = async (
   let res: string | undefined
 
   if (createLinks.length > 0) {
-    ;[res] = await Promise.all(
-      createLinks.map(async (link) => {
-        const fileHash = link.hash
+    createLinks.map(async (link) => {
+      const fileHash = link.hash
 
-        const filePath = await getKv(fileHash)
-        if (!filePath) {
-          return '本地临时文件存储未找到, 请重新上传'
+      const filePath = await getKv(fileHash)
+      if (!filePath) {
+        res = '本地临时文件存储未找到, 请重新上传'
+        return
+      }
+      const fileName = filePath.split('/').pop()
+
+      // 上传文件到 S3
+      const s3Key = `patch/${patchId}/${fileName}`
+      // await uploadFileToS3(s3Key, filePath);
+
+      await prisma.patch_resource_link.create({
+        data: {
+          type: link.type,
+          hash: link.hash,
+          content: link.content,
+          patch_id: patchId,
+          patch_resource_id: patchResourceId
         }
-        const fileName = filePath.split('/').pop()
-
-        // 上传文件到 S3
-        const s3Key = `patch/${patchId}/${fileName}`
-        // await uploadFileToS3(s3Key, filePath);
-
-        await prisma.patch_resource_link.create({
-          data: {
-            type: link.type,
-            hash: link.hash,
-            content: link.content,
-            patch_id: patchId,
-            patch_resource_id: patchResourceId
-          }
-        })
       })
-    )
+    })
   }
 
   if (updateLinks.length > 0) {
-    ;[res] = await Promise.all(
-      updateLinks.map(async (link) => {
-        const fileHash = link.hash
+    updateLinks.map(async (link) => {
+      const fileHash = link.hash
 
-        const filePath = await getKv(fileHash)
-        if (!filePath) {
-          return '本地临时文件存储未找到, 请重新上传'
+      const filePath = await getKv(fileHash)
+      if (!filePath) {
+        res = '本地临时文件存储未找到, 请重新上传'
+        return
+      }
+      const fileName = filePath.split('/').pop()
+
+      // Upload new file to S3
+      // const oldS3Key = `patch/${patchId}/${fileName}`
+      // const newS3Key = `patch/${patchId}/${fileName}`
+
+      // await Promise.all([
+      //   deleteFileFromS3(oldS3Key),
+      //   uploadFileToS3(newS3Key, filePath)
+      // ])
+
+      await prisma.patch_resource_link.update({
+        where: {
+          id: link.id
+        },
+        data: {
+          type: link.type,
+          hash: link.hash,
+          content: link.content
         }
-        const fileName = filePath.split('/').pop()
-
-        // Upload new file to S3
-        // const oldS3Key = `patch/${patchId}/${fileName}`
-        // const newS3Key = `patch/${patchId}/${fileName}`
-
-        // await Promise.all([
-        //   deleteFileFromS3(oldS3Key),
-        //   uploadFileToS3(newS3Key, filePath)
-        // ])
-
-        await prisma.patch_resource_link.update({
-          where: {
-            id: link.id
-          },
-          data: {
-            type: link.type,
-            hash: link.hash,
-            content: link.content
-          }
-        })
       })
-    )
+    })
   }
 
   if (res) {
