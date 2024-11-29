2a5f744d7dc4b0eafa2ba1a721c7c34ecfab51c6
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-11-29 21:15:29 +0800

    feat: add a cron job to reset user daily limit

---
+++ server/cron.ts
@@ -0,0 +1,16 @@
+import { prisma } from '~/prisma'
+import cron from 'node-cron'
+
+const resetDailyTask = cron.schedule('0 0 * * *', async () => {
+  await prisma.user.updateMany({
+    data: {
+      daily_image_count: 0,
+      daily_check_in: 0,
+      daily_upload_size: 0
+    }
+  })
+})
+
+export const useKUNGalgameTask = () => {
+  resetDailyTask
+}
