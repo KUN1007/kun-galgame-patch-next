ee662a405c08ab7781ca5c2c8734131cefdb9c26
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-12-07 17:36:00 +0800

    feat: edit, auth page SEO metadata

--- app/auth/forgot/page.tsx
+++ app/auth/forgot/page.tsx
@@ -1,4 +1,8 @@
 import { ForgotPassword } from '~/components/forgot/Forgot'
+import { kunMetadata } from './metadata'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = kunMetadata
 
 export default function ResetPassword() {
   return <ForgotPassword />
--- app/edit/create/page.tsx
+++ app/edit/create/page.tsx
@@ -1,4 +1,8 @@
 import { CreatePatch } from '~/components/edit/create/CreatePatch'
+import { kunMetadata } from './metadata'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = kunMetadata
 
 export default function Create() {
   return (
