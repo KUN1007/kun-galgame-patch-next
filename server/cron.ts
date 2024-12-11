6ec26eee146c71e2cd9c8a2c5b1214ffbbced836
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-12-11 22:29:50 +0800

    feat: resetDailyTask

--- server/cron.ts
+++ server/cron.ts
@@ -1,16 +1,7 @@
-import { prisma } from '~/prisma'
-import cron from 'node-cron'
-
-const resetDailyTask = cron.schedule('0 0 * * *', async () => {
-  await prisma.user.updateMany({
-    data: {
-      daily_image_count: 0,
-      daily_check_in: 0,
-      daily_upload_size: 0
-    }
-  })
-})
+import { resetDailyTask } from './tasks/resetDailyTask'
+import { setCleanupTask } from './tasks/setCleanupTask'
 
 export const setKUNGalgameTask = () => {
   resetDailyTask
+  setCleanupTask
 }
