24dd72f664c4f67bec120c78367d1df1c93195bb
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-12-12 19:09:45 +0800

    fix: upload banner error

--- app/api/edit/create.ts
+++ app/api/edit/create.ts
@@ -11,20 +11,7 @@ export const createPatch = async (
 ) => {
   const { name, vndbId, alias, banner, introduction, released } = input
 
-  const currentId: { last_value: number }[] =
-    await prisma.$queryRaw`SELECT last_value FROM patch_id_seq`
-  const newId = Number(currentId[0].last_value) + 1
-
   const bannerArrayBuffer = banner as ArrayBuffer
-  const res = await uploadPatchBanner(bannerArrayBuffer, newId)
-  if (!res) {
-    return '上传图片错误, 未知错误'
-  }
-  if (typeof res === 'string') {
-    return res
-  }
-
-  const imageLink = `${process.env.KUN_VISUAL_NOVEL_IMAGE_BED_URL}/patch/${newId}/banner/banner.avif`
 
   return await prisma.$transaction(async (prisma) => {
     const patch = await prisma.patch.create({
@@ -34,9 +21,26 @@ export const createPatch = async (
         alias: alias ? alias : [],
         introduction,
         user_id: uid,
-        banner: imageLink,
+        banner: '',
         released
       }
+    })
+
+    const newId = patch.id
+
+    const res = await uploadPatchBanner(bannerArrayBuffer, newId)
+    if (!res) {
+      return '上传图片错误, 未知错误'
+    }
+    if (typeof res === 'string') {
+      return res
+    }
+
+    const imageLink = `${process.env.KUN_VISUAL_NOVEL_IMAGE_BED_URL}/patch/${newId}/banner/banner.avif`
+
+    await prisma.patch.update({
+      where: { id: newId },
+      data: { banner: imageLink }
     })
 
     await prisma.user.update({
@@ -50,7 +54,7 @@ export const createPatch = async (
     await prisma.user_patch_contribute_relation.create({
       data: {
         user_id: uid,
-        patch_id: patch.id
+        patch_id: newId
       }
     })
 
@@ -60,10 +64,10 @@ export const createPatch = async (
         type: 'galgame',
         content: name,
         user_id: uid,
-        patch_id: patch.id
+        patch_id: newId
       }
     })
 
-    return patch.id
+    return newId
   })
 }
