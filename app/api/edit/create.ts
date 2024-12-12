19a86ffb10d1e42935dcf593c0f729ead866adf2
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-12-13 00:35:01 +0800

    fix: publish galgame banner error

--- app/api/edit/create.ts
+++ app/api/edit/create.ts
@@ -11,60 +11,66 @@ export const createPatch = async (
 ) => {
   const { name, vndbId, alias, banner, introduction, released } = input
 
-  const lastPatch = await prisma.patch.findFirst({
-    orderBy: { id: 'desc' }
-  })
-  const nextId = lastPatch ? lastPatch.id + 1 : 1
-
   const bannerArrayBuffer = banner as ArrayBuffer
-  const res = await uploadPatchBanner(bannerArrayBuffer, nextId)
-  if (!res) {
-    return '上传图片错误, 未知错误'
-  }
-  if (typeof res === 'string') {
-    return res
-  }
 
-  const imageLink = `${process.env.KUN_VISUAL_NOVEL_IMAGE_BED_URL}/patch/${nextId}/banner/banner.avif`
+  return await prisma.$transaction(
+    async (prisma) => {
+      const patch = await prisma.patch.create({
+        data: {
+          name,
+          vndb_id: vndbId,
+          alias: alias ? alias : [],
+          introduction,
+          user_id: uid,
+          banner: '',
+          released
+        }
+      })
 
-  return await prisma.$transaction(async (prisma) => {
-    const patch = await prisma.patch.create({
-      data: {
-        name,
-        vndb_id: vndbId,
-        alias: alias ? alias : [],
-        introduction,
-        user_id: uid,
-        banner: imageLink,
-        released
+      const newId = patch.id
+
+      const res = await uploadPatchBanner(bannerArrayBuffer, newId)
+      if (!res) {
+        return '上传图片错误, 未知错误'
       }
-    })
-
-    await prisma.user.update({
-      where: { id: uid },
-      data: {
-        daily_image_count: { increment: 1 },
-        moemoepoint: { increment: 3 }
+      if (typeof res === 'string') {
+        return res
       }
-    })
 
-    await prisma.user_patch_contribute_relation.create({
-      data: {
-        user_id: uid,
-        patch_id: patch.id
-      }
-    })
+      const imageLink = `${process.env.KUN_VISUAL_NOVEL_IMAGE_BED_URL}/patch/${newId}/banner/banner.avif`
 
-    await prisma.patch_history.create({
-      data: {
-        action: 'create',
-        type: 'galgame',
-        content: name,
-        user_id: uid,
-        patch_id: patch.id
-      }
-    })
+      await prisma.patch.update({
+        where: { id: newId },
+        data: { banner: imageLink }
+      })
 
-    return patch.id
-  })
+      await prisma.user.update({
+        where: { id: uid },
+        data: {
+          daily_image_count: { increment: 1 },
+          moemoepoint: { increment: 3 }
+        }
+      })
+
+      await prisma.user_patch_contribute_relation.create({
+        data: {
+          user_id: uid,
+          patch_id: newId
+        }
+      })
+
+      await prisma.patch_history.create({
+        data: {
+          action: 'create',
+          type: 'galgame',
+          content: name,
+          user_id: uid,
+          patch_id: newId
+        }
+      })
+
+      return newId
+    },
+    { timeout: 60000 }
+  )
 }
