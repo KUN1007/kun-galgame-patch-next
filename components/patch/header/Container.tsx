877c6beff3e011d4c2227bb450dd6943e70483bb
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-01-25 23:12:40 +0800

    feat: add KunAutoImageViewer

--- components/patch/header/Container.tsx
+++ components/patch/header/Container.tsx
@@ -4,12 +4,13 @@ import { useEffect } from 'react'
 import { Card, CardBody } from '@nextui-org/card'
 import { Divider } from '@nextui-org/divider'
 import { useRewritePatchStore } from '~/store/rewriteStore'
+import { KunAutoImageViewer } from '~/components/kun/image-viewer/AutoImageViewer'
 import { KunCardStats } from '~/components/kun/CardStats'
 import { PatchHeader } from './Header'
 import { PatchHeaderTabs } from './Tabs'
 import { formatDistanceToNow } from '~/utils/formatDistanceToNow'
 import { Tags } from './Tags'
-import { Image } from '@nextui-org/image'
+import Image from 'next/image'
 import { KunUser } from '~/components/kun/floating-card/KunUser'
 import { ButtonList } from './ButtonList'
 import type { Patch } from '~/types/api/patch'
@@ -36,16 +37,18 @@ export const PatchHeaderContainer = ({ patch }: PatchHeaderProps) => {
   return (
     <>
       <div className="relative w-full sm:h-[512px]">
+        <KunAutoImageViewer />
+        {/* Why use unoptimized? Production env unknown error */}
         <Image
           src={patch.banner}
           alt={patch.name}
+          className="absolute top-0 left-0 object-cover size-full rounded-t-2xl rounded-b-3xl"
+          fill
           sizes="100vw"
-          classNames={{
-            wrapper:
-              '-z-10 size-full overflow-hidden sm:absolute sm:top-0 sm:left-0',
-            img: 'w-full'
-          }}
+          priority
+          unoptimized
         />
+
         <PatchHeader patch={patch} />
         <Card className="w-full rounded-none shadow-lg rounded-b-2xl bg-background/70 backdrop-blur-xl sm:absolute sm:bottom-0">
           <CardBody>
