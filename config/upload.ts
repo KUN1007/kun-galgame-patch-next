aec93b2a72f750a8629daef29631af82e6f09fa9
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-01-29 18:52:19 +0800

    mod: upload chunk size

--- config/upload.ts
+++ config/upload.ts
@@ -1,4 +1,4 @@
-export const CHUNK_SIZE = 5 * 1024 * 1024
+export const CHUNK_SIZE = 20 * 1024 * 1024
 export const UPLOAD_DIR = 'uploads/resource'
 export const MAX_FILE_SIZE = 1 * 1024 * 1024 * 1024
 
