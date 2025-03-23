65e33bd105552a75ffe3671cf28b9482489ad292
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-03-23 23:09:43 +0800

    fix: allow hikari api cors

--- app/api/hikari/route.ts
+++ app/api/hikari/route.ts
@@ -6,8 +6,8 @@ import type { HikariResponse } from './type'
 
 // Define allowed origins
 const allowedOrigins = [
-  'http://localhost',
-  'http://127.0.0.1',
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
   'https://www.hikarinagi.com'
 ]
 
