332ba2244e0b9b24c1a15d0aab563395bf82f3dc
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-06-02 22:42:43 +0800

    fix: ReferenceError: __dirname is not defined

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,15 @@
-import path from 'path'
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default {
   apps: [
     {
       name: 'kun-visual-novel-patch',
       port: 2333,
-      cwd: path.join(__dirname),
+      cwd: join(__dirname),
       instances: 1,
       autorestart: true,
       watch: false,
